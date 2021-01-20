9fc5bcc19a7c5146e0a457bed8cedbad4a3c38d1
Clara CHAOUAT <chaouatclara337@hotmail.com>
2021-01-20 17:50:52 +0200

    css related question

--- animations.js
+++ animations.js
@@ -1,27 +1,15 @@
-let calendar = document.getElementById('calendar')
-let circleIcon = document.getElementById('circle')
+let calendar = document.getElementById('calendar');
+let circleIcon = document.getElementById('circle');
+let searchIcon = document.getElementById('search');
 
-let clickHandlerCalendar = function () {
-  console.log('Click just happened')
-  if (this.classList.contains('active')) {
-    console.log('This class is active')
-    this.classList.remove('active')
-    circleIcon.classList.add('active')
-  } else {
-    console.log('There is no active class!')
-  }
-}
 
-calendar.addEventListener('click', clickHandlerCalendar)
+// First Icon event handling
+calendar.addEventListener('click', () => {
+  calendar.classList.toggle('active');
+  circleIcon.classList.toggle('active');
+});
 
-let clickHandlerCircle = function () {
-  console.log('Click event on circle is ok!')
-  if (circleIcon.classList.contains('active')) {
-    circleIcon.classList.remove('active')
-    calendar.classList.add('active')
-  } else {
-    console.log('Issue on clickHandlerCircle')
-  }
-}
-
-circleIcon.addEventListener('click', clickHandlerCircle)
+circleIcon.addEventListener('click',() => {
+  circleIcon.classList.toggle('active');
+  calendar.classList.toggle('active');
+})
