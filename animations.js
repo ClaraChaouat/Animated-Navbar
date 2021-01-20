823c6c360aae67fde94ec81edf7f0a30382f0cd9
Clara CHAOUAT <chaouatclara337@hotmail.com>
2021-01-21 00:21:17 +0200

    Added "motion" animation on calendar

--- animations.js
+++ animations.js
@@ -1,15 +1,14 @@
-let calendar = document.getElementById('calendar');
-let circleIcon = document.getElementById('circle');
-let searchIcon = document.getElementById('search');
-
+let calendar = document.getElementById('calendar')
+let circleIcon = document.getElementById('circle')
+let searchIcon = document.getElementById('search')
 
 // First Icon event handling
 calendar.addEventListener('click', () => {
-  calendar.classList.toggle('active');
-  circleIcon.classList.toggle('active');
-});
+  calendar.classList.toggle('active')
+  circleIcon.classList.toggle('active')
+})
 
-circleIcon.addEventListener('click',() => {
-  circleIcon.classList.toggle('active');
-  calendar.classList.toggle('active');
+circleIcon.addEventListener('click', () => {
+  circleIcon.classList.toggle('active')
+  calendar.classList.toggle('active')
 })
